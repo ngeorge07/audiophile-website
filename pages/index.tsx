f019ecf07c324e37252ecb90c70981f88f1f37a1
George <georgenico9@gmail.com>
2022-09-15 00:45:05 +0200

    perf: configure tailwind fonts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,20 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <h1>Morbi interdum</h1>
+      <h2>Morbi interdum</h2>
+      <h3>Morbi interdum</h3>
+      <h4>Morbi interdum</h4>
+      <h5>Morbi interdum</h5>
+      <h6>Morbi interdum</h6>
+
+      <p className="font-overline">Morbi interdum</p>
+      <p className="font-subtitle">Morbi interdum</p>
+      <p className="font-body">Morbi interdum</p>
+    </>
+  );
 };
 
 export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,78 @@ module.exports = {
       accent2: '#fbaf85',
 
       background: '#FAFAFA',
+      black: '#000',
+      white: '#fff',
+    },
+    fontFamily: {
+      manrope: 'Manrope',
+    },
+    fontSize: {
+      h1: [
+        '56px',
+        {
+          letterSpacing: '2px',
+          lineHeight: '58px',
+        },
+      ],
+
+      h2: [
+        '40px',
+        {
+          letterSpacing: '1.5px',
+          lineHeight: '44px',
+        },
+      ],
+
+      h3: [
+        '32px',
+        {
+          letterSpacing: '1.15px',
+          lineHeight: '36px',
+        },
+      ],
+
+      h4: [
+        '28px',
+        {
+          letterSpacing: '2px',
+          lineHeight: '38px',
+        },
+      ],
+
+      h5: [
+        '24px',
+        {
+          letterSpacing: '1.7px',
+          lineHeight: '33px',
+        },
+      ],
+
+      h6: [
+        '18px',
+        {
+          letterSpacing: '1.3px',
+          lineHeight: '25px',
+        },
+      ],
+
+      p: ['15px', '25px'],
+
+      overline: [
+        '14px',
+        {
+          letterSpacing: '10px',
+          lineHeight: '19px',
+        },
+      ],
+
+      subtitle: [
+        '13px',
+        {
+          letterSpacing: '1px',
+          lineHeight: '25px',
+        },
+      ],
     },
   },
   plugins: [],
