754f588b517d202acc495f5cb4d8a936856237cc
George <georgenico9@gmail.com>
2022-10-10 02:02:21 +0200

    fix: add default values for product card in storybook

--- components/cards/product-card/ProductCard.mocks.ts
+++ components/cards/product-card/ProductCard.mocks.ts
@@ -1,6 +1,14 @@
 import { IProductCard } from './ProductCard';
 
-const base: IProductCard = {};
+const base: IProductCard = {
+  slug: 'xx99-mark-two-headphones',
+  name: 'XX99 Mark II Headphones',
+  description:
+    'The new XX99 Mark II headphones is the pinnacle of pristine audio. It redefines your premium headphone experience by reproducing the balanced depth and precision of studio-quality sound.',
+  category: 'headphones',
+  newProduct: true,
+  index: 1,
+};
 
 export const mockProductCardProps = {
   base,
