4ade7b5648260b050ec1c0ea846274973937ef62
George <georgenico9@gmail.com>
2022-10-13 16:51:03 +0200

    fix: add defaults for suggested product card story

--- components/cards/product-suggested-card/ProductSuggestedCard.mocks.ts
+++ components/cards/product-suggested-card/ProductSuggestedCard.mocks.ts
@@ -1,6 +1,10 @@
 import { IProductSuggestedCard } from './ProductSuggestedCard';
 
-const base: IProductSuggestedCard = {};
+const base: IProductSuggestedCard = {
+  slug: 'xx99-mark-one-headphones',
+  name: 'XX99 Mark I',
+  category: 'headphones',
+};
 
 export const mockProductSuggestedCardProps = {
   base,
